085c7f11c2a6fc14408e3079ca22a8663bb6cdde
Lukas Juhrich <lukas.juhrich@agdsn.de>
2021-03-03 20:38:22 +0100

    enforce black codestyle

--- lonk/app.py
+++ lonk/app.py
@@ -1,29 +1,29 @@
 from flask import Flask, redirect
 
-app = Flask('lonk')
+app = Flask("lonk")
 
 
-@app.route('/')
+@app.route("/")
 def index():
     return "Welcome to LONK."
 
 
-@app.route('/<shortname>')
+@app.route("/<shortname>")
 def resolve_shortlink(shortname: str):
-    if shortname == 'register':
-        return redirect('https://agdsn.de/sipa/register', 301)
+    if shortname == "register":
+        return redirect("https://agdsn.de/sipa/register", 301)
     else:
         return f"There is no redirect named '{shortname}'.", 404
     # TODO implement proper redirect from DB
 
 
-@app.route('/_admin')
+@app.route("/_admin")
 def admin_overview():
     # TODO implement admin landing page
     pass
 
 
-@app.route('/_admin/create', methods=['GET', 'POST'])
+@app.route("/_admin/create", methods=["GET", "POST"])
 def create():
     # TODO implement redirect creation
     pass
--- tests/test_root_access.py
+++ tests/test_root_access.py
@@ -3,16 +3,16 @@ from flask.testing import FlaskClient
 
 
 def test_root(client: FlaskClient):
-    resp = client.get('/')
+    resp = client.get("/")
     assert resp.status_code == 200
-    assert "welcome" in resp.data.decode('utf-8').lower()
+    assert "welcome" in resp.data.decode("utf-8").lower()
 
 
-@pytest.mark.parametrize("url", ['foo', 'bar', 'Hans', 'Peter', 'nonexistent'])
+@pytest.mark.parametrize("url", ["foo", "bar", "Hans", "Peter", "nonexistent"])
 def test_nonexistent_redirect(client: FlaskClient, url):
     resp = client.get(f"/{url}")
     assert resp.status_code == 404
-    assert "there is no redirect" in resp.data.decode('utf-8').lower()
+    assert "there is no redirect" in resp.data.decode("utf-8").lower()
 
 
 def test_existent_redirect(client: FlaskClient):
