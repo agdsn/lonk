32354577ab3fd9145938ac4f5ae97452da4d4f7f
Lukas Juhrich <lukas.juhrich@agdsn.de>
2021-03-03 20:07:39 +0100

    Test for redirect URLs

--- tests/test_root_access.py
+++ tests/test_root_access.py
@@ -6,3 +6,16 @@ def test_root(client: FlaskClient):
     resp = client.get('/')
     assert resp.status_code == 200
     assert "welcome" in resp.data.decode('utf-8').lower()
+
+
+@pytest.mark.parametrize("url", ['foo', 'bar', 'Hans', 'Peter', 'nonexistent'])
+def test_nonexistent_redirect(client: FlaskClient, url):
+    resp = client.get(f"/{url}")
+    assert resp.status_code == 404
+    assert "there is no redirect" in resp.data.decode('utf-8').lower()
+
+
+def test_existent_redirect(client: FlaskClient):
+    resp = client.get(f"/register")
+    assert resp.status_code == 301  # Moved permanently
+    assert resp.location == "https://agdsn.de/sipa/register"
