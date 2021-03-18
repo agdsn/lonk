4119933da3cae3b71a90acae586abda6394c42c2
Lukas Juhrich <lukas.juhrich@agdsn.de>
2021-03-18 20:40:40 +0100

    Defer route configuration to runtime instead of import-time

--- lonk/app.py
+++ lonk/app.py
@@ -11,34 +11,35 @@ class Lonk(Flask):
         self.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
         self.config.setdefault('SQLALCHEMY_DATABASE_URI', 'sqlite:///:memory:')
         db.init_app(self)
+        register_routes(self)
+
+
+
+
+def register_routes(app):
+    @app.route("/")
+    def index():
+        return "Welcome to LONK."
+
+    @app.route("/<shortname>")
+    def resolve_shortlink(shortname: str) -> FlaskResponse:
+        if not shortname:
+            abort(400)
+
+        if url := try_lookup_link(shortname):
+            return redirect(url, 301)
+
+        return f"There is no redirect named '{shortname}'.", 404
+
+    @app.route("/_admin")
+    def admin_overview():
+        # TODO implement admin landing page
+        pass
+
+    @app.route("/_admin/create", methods=["GET", "POST"])
+    def create():
+        # TODO implement redirect creation
+        pass
 
 
 app = Lonk("lonk")
-
-
-@app.route("/")
-def index():
-    return "Welcome to LONK."
-
-
-@app.route("/<shortname>")
-def resolve_shortlink(shortname: str) -> FlaskResponse:
-    if not shortname:
-        abort(400)
-
-    if url := try_lookup_link(shortname):
-        return redirect(url, 301)
-
-    return f"There is no redirect named '{shortname}'.", 404
-
-
-@app.route("/_admin")
-def admin_overview():
-    # TODO implement admin landing page
-    pass
-
-
-@app.route("/_admin/create", methods=["GET", "POST"])
-def create():
-    # TODO implement redirect creation
-    pass
